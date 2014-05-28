c20694e409465c58790f2783905344f060bf9311
nqounet <nqou.net@gmail.com>
2014-05-29 01:31:43 +0900

    slide-1-2のタスクを作成 && 不要になったファイルを削除(use-gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -14,4 +14,17 @@ gulp.task('slide-1-1', function(){
         .pipe(gulp.dest('build'));
 });
 
+gulp.task('slide-1-2', function(){
+    return gulp.src([
+            'title.md',
+            'about_perl_entrance.md',
+            'about_perl_language.md',
+            'more_study.md',
+            'build_perl.md',
+            'hello_world.md'
+        ])
+        .pipe(concat('slide-1-2.md'))
+        .pipe(gulp.dest('build'));
+});
+
 gulp.task('default', ['slide-1-1', 'slide-1-2']);
